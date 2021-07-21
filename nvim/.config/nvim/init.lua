cbd7cd43e0d2f63ea569dfcf8e922bce0f2c0e9f
marjacob <marjacob@users.noreply.github.com>
2021-07-21 20:20:05 +0200

    Switch to init.lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,20 @@
+vim.opt.clipboard:prepend("unnamed,unnamedplus")
+vim.opt.colorcolumn = "+1"
+vim.opt.foldenable = false
+vim.opt.hidden = true
+vim.opt.ignorecase = true
+vim.opt.infercase = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.numberwidth = 5
+vim.opt.report = 0
+vim.opt.scrolloff = 5
+vim.opt.shortmess:append({ I = true })
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+vim.opt.timeout = false
+vim.opt.virtualedit = "block"
+vim.opt.wildmode = "list:longest,full"
+vim.opt.wrap = false
