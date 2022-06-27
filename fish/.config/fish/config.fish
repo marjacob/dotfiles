066fda6be0bbe8b916bed85a00b59e2bdae09a2a
marjacob <marjacob@users.noreply.github.com>
2022-06-28 01:46:00 +0200

    fish: Apply configuration in interactive mode only

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,12 @@
-fish_add_path -g "{$HOME}/.cargo/bin"
-fish_add_path -g "{$HOME}/.local/bin"
+if status is-interactive
+    fish_add_path -g "{$HOME}/.cargo/bin"
+    fish_add_path -g "{$HOME}/.local/bin"
 
-set -gx BROWSER firefox
-set -gx CC clang
-set -gx CXX clang++
-set -gx EDITOR vim
-set -gx PREFIX {$HOME}/.local
-set -gx SVDIR {$HOME}/.local/var/service
-set -gx VISUAL {$EDITOR}
+    set -gx BROWSER firefox
+    set -gx CC clang
+    set -gx CXX clang++
+    set -gx EDITOR vim
+    set -gx PREFIX {$HOME}/.local
+    set -gx SVDIR {$HOME}/.local/var/service
+    set -gx VISUAL {$EDITOR}
+end
