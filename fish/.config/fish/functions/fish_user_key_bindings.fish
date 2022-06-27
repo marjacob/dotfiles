4748bd785f4c08f457f3f4d8cc7c4d9f7ae8f9c6
marjacob <marjacob@users.noreply.github.com>
2022-06-27 18:07:38 +0200

    fish: Use Vi mode

---
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,15 @@
+function fish_user_key_bindings
+    # Use Vi mode.
+    # https://fishshell.com/docs/current/interactive.html#visual-mode
+    fish_vi_key_bindings
+
+    # Use jk as an alternative escape sequence.
+    # https://stackoverflow.com/a/49040911
+    bind -M insert -m default jk backward-char force-repaint
+
+    # Use Ctrl-O to accept autosuggestions.
+    # https://github.com/fish-shell/fish-shell/issues/3541
+    for mode in default insert visual
+        bind -M $mode \co forward-char
+    end
+end
