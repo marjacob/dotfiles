fe60cde07d3fcb82b9bc2fc5a606393f603f5a94
marjacob <marjacob@users.noreply.github.com>
2022-07-03 21:45:42 +0200

    fish: Add GOPATH for Go

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,14 @@
 if status is-interactive
-    fish_add_path -g "{$HOME}/.cargo/bin"
-    fish_add_path -g "{$HOME}/.local/bin"
-
     set -gx BROWSER firefox
     set -gx CC clang
     set -gx CXX clang++
     set -gx EDITOR vim
+    set -gx GOPATH {$HOME}/.go
     set -gx PREFIX {$HOME}/.local
     set -gx SVDIR {$HOME}/.local/var/service
     set -gx VISUAL {$EDITOR}
+
+    fish_add_path {$GOPATH}/bin
+    fish_add_path {$HOME}/.cargo/bin
+    fish_add_path {$HOME}/.local/bin
 end
